d6cfe96de6dc21337e68d4a0b7019d3a8a2158dd
Minh Tue Vo <mvo@box.com>
2015-06-08 17:49:18 -0400

    First commit

---
+++ hatSort.py
@@ -0,0 +1,19 @@
+def hatSort(student):
+	if not student.magic():
+		return "Hogwarts is not for Muggles"
+
+	brave = student.brave()
+	cunning = student.cunning()
+	witty = student.witty()
+
+	if (brave + cunning + witty > 1):
+		return student.choice()
+
+	if brave:
+		return "Gryffindor"
+	elif cunning:
+		return "Slytherine"
+	elif witty:
+		return "Ravenclaw"
+	else:
+		return "Hufflepuff"	
