3d0184b540a87d4d2cd78d698652ee0bd3f76a24
Minh Tue Vo <mvo@box.com>
2015-06-08 17:55:17 -0400

    Adding function doc

--- hatSort.py
+++ hatSort.py
@@ -1,19 +1,22 @@
+
 def hatSort(student):
-	if not student.magic():
-		return "Hogwarts is not for Muggles"
+    """ The magical working under the hat """
+    
+    if not student.magic():
+        return "Hogwarts is not for Muggles"
 
-	brave = student.brave()
-	cunning = student.cunning()
-	witty = student.witty()
+    brave = student.brave()
+    cunning = student.cunning()
+    witty = student.witty()
 
-	if (brave + cunning + witty > 1):
-		return student.choice()
+    if (brave + cunning + witty > 1):
+        return student.choice()
 
-	if brave:
-		return "Gryffindor"
-	elif cunning:
-		return "Slytherine"
-	elif witty:
-		return "Ravenclaw"
-	else:
-		return "Hufflepuff"	
+    if brave:
+        return "Gryffindor!"
+    elif cunning:
+        return "Slytherine!"
+    elif witty:
+        return "Ravenclaw!"
+    else:
+        return "Hufflepuff!" 
